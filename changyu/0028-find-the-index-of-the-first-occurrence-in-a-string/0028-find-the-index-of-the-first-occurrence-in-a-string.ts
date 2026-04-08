190840cbec2aef43f7ee3eaa32296c612e4c75bd
여찬규 <dev.cgyeo@gmail.com>
2026-04-08 20:56:15 +0900

    💡 [LTC] Two Pointers: 0028-find-the-index-of-the-first-occurrence-in-a-string
    - Time: 0 ms (100%)
    - Space: 55.7 MB (56.54%)
    - Difficulty: Easy
    - Date: 04-08-2026

--- changyu/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.ts
+++ changyu/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.ts
@@ -1,13 +1,3 @@
 function strStr(haystack: string, needle: string): number {
-    for (let i = 0; i < haystack.length; i++) {
-        for (let j = 0; j < needle.length; j++) {
-            if (haystack[i + j] !== needle[j]) break;
-
-            if (j === needle.length - 1) {
-                return i;
-            }
-        }
-    }
-
-    return -1;
+	return haystack.indexOf(needle);
 };
