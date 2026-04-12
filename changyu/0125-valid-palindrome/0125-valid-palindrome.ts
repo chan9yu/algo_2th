d18cc126ad7f5660b17aa395c596455791a5a6cc
여찬규 <dev.cgyeo@gmail.com>
2026-04-12 18:41:41 +0900

    💡 [LTC] Two Pointers: 0125-valid-palindrome
    - Time: 6 ms (45.85%)
    - Space: 62.3 MB (22.83%)
    - Difficulty: Easy
    - Date: 04-12-2026

--- changyu/0125-valid-palindrome/0125-valid-palindrome.ts
+++ changyu/0125-valid-palindrome/0125-valid-palindrome.ts
@@ -1,27 +1,27 @@
 function isPalindrome(s: string): boolean {
-    const str = s.toLowerCase();
-    let left = 0;
-    let right = str.length - 1;
-
-    while (left < right) {
-        if (!/[a-z0-9]/.test(str[left])) {
-            left += 1;
-            continue;
-        }
-
-        if (!/[a-z0-9]/.test(str[right])) {
-            right -= 1;
-            continue;
-        }
-
-        if (str[left] === str[right]) {
-            left += 1;
-            right -= 1;
-            continue;
-        }
-
-        return false;
-    }
-
-    return true;
+	const str = s.toLowerCase();
+	let	left = 0;
+	let right = s.length - 1;
+	
+	while (left < right) {
+		if (!/[a-z0-9]/.test(str[left])) {
+			left += 1;
+			continue;
+		}
+		
+		if (!/[a-z0-9]/.test(str[right])) {
+			right -= 1;
+			continue;
+		}
+		
+		if (str[left] === str[right]) {
+			left += 1;
+			right -= 1;
+			continue;
+		}
+		
+		return false;
+	}
+	
+	return true;
 };
